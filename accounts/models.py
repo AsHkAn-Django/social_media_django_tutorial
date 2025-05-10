56bfbeb9bc199cc458067dc3eb975ad6eb6101fd
AsHkaN <ashkan_ahrari@yahoo.com>
2025-05-10 18:08:59 +0300

    customUser model created

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    image = models.ImageField(upload_to="users/%Y/%m/%d", blank=True)
+    
