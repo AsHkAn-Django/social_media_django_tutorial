0dcef1081d8af320ba30e548ba2535ece6767820
AsHkaN <ashkan_ahrari@yahoo.com>
2025-05-15 14:14:46 +0300

    like and unlike system added to veiw and template

--- myApp/urls.py
+++ myApp/urls.py
@@ -5,4 +5,8 @@ app_name = 'myApp'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
+    path('posts/add', views.AddPostCreateView.as_view(), name='add_post'),
+    path('posts/', views.PostListView.as_view(), name='post_list'),
+    path('posts/feed', views.FeedPostListView.as_view(), name='feed_post_list'),
+    path('posts/like/<int:pk>/', views.like_post, name='like_post'),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
+from .models import Post, Comment, Like
+from .forms import PostForm, CommentForm
+from django.shortcuts import get_object_or_404, redirect
 
 
 
@@ -8,6 +11,43 @@ class IndexView(generic.TemplateView):
     
     
 
+class AddPostCreateView(generic.CreateView):
+    model = Post
+    template_name = "myApp/add_post.html"
+    form_class = PostForm
+    success_url = reverse_lazy('myApp:post_list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 
+class PostListView(generic.ListView):
+    model = Post
+    template_name = "myApp/post_list.html"
+    context_object_name = 'posts'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['liked_ids'] = Like.objects.filter(user=self.request.user).values_list('post_id', flat=True)
+        return context
+    
+    
+    
+
+class FeedPostListView(generic.ListView):
+    template_name = "myApp/feed_post_list.html"
+    context_object_name = "posts"
+
+
+
+def like_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    like_exist = Like.objects.filter(user=request.user, post=post)
+    if like_exist:
+        like_exist.delete()
+    else:
+        Like.objects.create(user=request.user, post=post)
+    return redirect('myApp:post_list')
+        
